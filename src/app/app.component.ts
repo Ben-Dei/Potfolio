7c807f644e1dc16395976f4f817fdd2ce83dda45
Ben Dei <Xelnarash@googlemail.com>
2024-12-07 13:16:32 +0100

    html and scss for main content finished & added 'why me' section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { NavbarComponent } from "./main-content/navbar/navbar.component";
+import { WhyMeComponent } from "./main-content/why-me/why-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet,
+    MainContentComponent, NavbarComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
