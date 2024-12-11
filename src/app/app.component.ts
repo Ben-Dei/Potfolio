339a0c0d28d19608c895e5ef003bb1ae3f80444c
Ben Dei <Xelnarash@googlemail.com>
2024-12-11 10:39:29 +0100

    added my skill section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { NavbarComponent } from "./main-content/navbar/navbar.component";
 import { WhyMeComponent } from "./main-content/why-me/why-me.component";
+import { MySkillsComponent } from "./main-content/my-skills/my-skills.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-    MainContentComponent, NavbarComponent, WhyMeComponent],
+    MainContentComponent, NavbarComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
