3d2ddb80f2f217773edae8f3b8a3499da0d96576
Ben Dei <Xelnarash@googlemail.com>
2024-12-16 11:06:12 +0100

    html and css from 'my projects' finished ; added and finished 'my references' in html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,14 @@ import { NavbarComponent } from "./main-content/navbar/navbar.component";
 import { WhyMeComponent } from "./main-content/why-me/why-me.component";
 import { MySkillsComponent } from "./main-content/my-skills/my-skills.component"
 import { MyProjectsComponent } from "./main-content/my-projects/my-projects.component";
+import { MyReferencesComponent } from './main-content/my-references/my-references.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-    MainContentComponent, NavbarComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+    MainContentComponent, NavbarComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent,
+  MyReferencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/main-content/my-references/my-references.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-references',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-references.component.html',
+  styleUrl: './my-references.component.scss'
+})
+export class MyReferencesComponent {
+
+}
