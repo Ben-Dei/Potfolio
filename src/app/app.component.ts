dee2ae7729bbc20203763a83384d7d4ddad108a7
Ben Dei <Xelnarash@googlemail.com>
2024-12-17 16:47:49 +0100

    added footer-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { NavbarComponent } from "./main-content/navbar/navbar.component";
 import { WhyMeComponent } from "./main-content/why-me/why-me.component";
 import { MySkillsComponent } from "./main-content/my-skills/my-skills.component"
 import { MyProjectsComponent } from "./main-content/my-projects/my-projects.component";
 import { MyReferencesComponent } from './main-content/my-references/my-references.component';
 import { ContactMeComponent } from "./main-content/contact-me/contact-me.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-    MainContentComponent, NavbarComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent,
-    MyReferencesComponent, ContactMeComponent],
+    MainContentComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent,
+    MyReferencesComponent, ContactMeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
